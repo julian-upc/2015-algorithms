72e047469782e66be1d84b1f9b1d56ca94e3e966
Felix Schröder <xef-xd@gmx.de>
2015-04-22 23:39:38 +0200

    homework for 22.4.2015
    pentomino class: rotation, flip, translation, etc.

--- pentominos.py
+++ pentominos.py
@@ -1,7 +1,6 @@
 import copy
 
-#incomplete
-# todo: change consistent to self.consistent
+#nontested
 class Pentomino(object):
     def __init__(self, name, coos):
         self.name = name
@@ -10,9 +9,9 @@ class Pentomino(object):
 
     #the comments are the version i would prefer, without input coo
     def normalize_coo(self, coo):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
-        if coo >=dim | coo<0:
+        if coo >=self.dim | coo<0:
             print "Invalid argument for coo"
         selbst = copy.deepcopy(self)
         for c in selbst.coos:
@@ -24,14 +23,14 @@ class Pentomino(object):
         #self.translate_by(max(selbst))
         
     def normalize(self):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
         self=nameInit[self.name]
 
     def flip(self, coo):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
-        if coo >=dim | coo<0:
+        if coo >=self.dim | coo<0:
             print "Invalid argument for coo"
         else:
             for c in self.coos:
@@ -42,11 +41,11 @@ class Pentomino(object):
         translate_coo(self,coo,1)
             
     #this method translates all coordinates in direction no. coo (starting with 0) by amount
-    #(negative amount yields the opposite direction
+    #(negative amount yields the opposite direction)
     def translate_coo(self, coo, amount):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
-        if coo >=dim | coo<0:
+        if coo >=self.dim | coo<0:
             print "Invalid argument for coo"
         else:
             for c in self.coos:
@@ -54,7 +53,7 @@ class Pentomino(object):
 
     #this method translates all coordinates of the pentomino by the vector by_vector
     def translate_by(self, by_vector):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
         if len(by_vector)!=self.dim:
             print "Invalid argument for by_vector"
@@ -64,40 +63,43 @@ class Pentomino(object):
                     c[i]+=by_vector[i]                    
                 #c= map(int.__add__,c,by_vector)
         
-    #Turn 90 degrees around [0,0]
-    # todo: around midpoint coos[2]
+    #Turn 90 degrees around midpoint: coos[2] for pentominos, coos[3] for hexominos, etc.
     def turn90(self):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
         if self.dim!=2:
             print "rotation only possible in 2D"
         else:
+            vec=copy.deepcopy(self.coos[len(self.coos)//2])
+            for i in range(self.dim):
+                vec[i]=-vec[i]
+            translate_by(vec)
             for c in self.coos:
                 tmp = c[0]
                 c[0]= -c[1]
                 c[1]= tmp
-                
+            translate_by(self.coos[len(self.coos)//2])
 
     def max(self):
-        if ~consistent():
+        if ~self.consistent():
             print "polyomino non consistent"
-        maxi = [0] * dim
+        maxi = [0] * self.dim
         for c in self.coos:
-            for i in range(dim):
+            for i in range(self.dim):
                 maxi[i] = max(c[i],maxi[i])
         return maxi
         
     # The hashfunction asserts that dimension is less or equal than 16, whilst strictly bigger than 0
     # It furtherly asserts that the coordinates are not greater than 255, and nonnegative.
     def __hash__(self):
-        if ~consistent():
+        if ~self.consistent():
             return -1
         hashcode = 0
         hashcode += hashName(self.name)
         hashcode += (self.dim-1)<<4
         e=8
         for c in self.coos:
-            for i in range(dim):
+            for i in range(self.dim):
                 hashcode += c[i]<<e
                 e+=8
         return hashcode
@@ -109,24 +111,24 @@ class Pentomino(object):
         elif ~other.consistent():
             print "polyomino non consistent"
         elif self.name!=other.name:
-            return false
+            return False
         elif self.dim!=other.dim:
-            return false
+            return False
         # self.coos == other.coos would compare pointers, not entries,
         # so we choose the difficult version
         elif len(self.coos)!=len(other.coos):
-            return false
+            return False
         else:
             for i in range(len(self.coos)):
                 if self.dim!=len(other.coos[i]):
-                    return false
+                    return False
                 elif len(self.coos[i])!=self.dim:
-                    return false
+                    return False
                 else:
                     for j in range(self.dim):
                         if self.coos[i][j]!=other.coos[i][j]:
-                            return false
-            return true
+                            return False
+            return True
 
     def representation(self):
         if ~self.consistent():
@@ -138,16 +140,59 @@ class Pentomino(object):
             selbst = copy.deepcopy(self)
             selbstnorm=nameInit(self.name)
             if len(self.coos)!=5:
-                return false
+                return False
             else:
                 for c in self.coos:
-                    if len(c) != dim:
-                    #TODO
-			return false
-	    
-            
+                    if len(c) != self.dim:
+			return False
+                for i in range(1,5):
+                    selbst.coos[i][0]=selbst.coos[i][0]-selbst.coos[0][0]
+                    selbst.coos[i][1]=selbst.coos[i][1]-selbst.coos[0][1]
+                selbst.coos[0][0]=0
+                selbst.coos[0][1]=0
+
+                #normalize selbst "by hand"
+                if coo2X(self.name):
+                    i=0
+                    while selbst.coos[1][0]!=1:
+                        i+=1
+                        if i>4 :
+                            return False
+                        for c in selbst.coos:
+                            tmp = c[0]
+                            c[0]= -c[1]
+                            c[1]= tmp
+                    for j in range(self.dim):
+                        selbst.coos[0][j]+=switchy(self.name)
+                else:
+                    while selbst.coos[1][1]!=1:
+                        i+=1
+                        if i>4 :
+                            return False
+                        for c in selbst.coos:
+                            tmp = c[0]
+                            c[0]= -c[1]
+                            c[1]= tmp
+                #selbst should now be the same as selbstnorm. We check that:
+                if selbst.name!=selbstnorm.name:
+                    return False
+                elif selbst.dim!=selbstnorm.dim:
+                    return False
+                # selbst.coos == selbstnorm.coos would compare pointers, not entries,
+                # so we choose the difficult version
+                elif len(selbst.coos)!=len(selbstnorm.coos):
+                    return False
+                else:
+                    for i in range(len(selbst.coos)):
+                        if selbst.dim!=len(selbstnorm.coos[i]):
+                            return False
+                        else:
+                            for j in range(selbst.dim):
+                                if selbst.coos[i][j]!=selbstnorm.coos[i][j]:
+                            return False
+                    return True
         except:
-            return false
+            return False
 
 class F(Pentomino):
     def __init__(self):
@@ -231,6 +276,32 @@ hashName = {
     "Z":11
 }
 
+switchy = {
+    "F":1
+    "T":2
+    "V":0
+    "W":0
+    "X":1
+    "Y":0
+    "Z":2
+}
+
+coo2X = {
+    "F":True
+    "I":False
+    "L":False
+    "P":False
+    "N":False
+    "T":True
+    "U":False
+    "V":True
+    "W":True
+    "X":True
+    "Y":True
+    "Z":True
+}
+
+
 class TileSet(object):
     def __init__(self, plist=[]):
         self.set = set()
@@ -241,8 +312,19 @@ class TileSet(object):
         return iter(self.set)
         
     def add(self, p):
-        tmp = 0 # TODO
-        self.set = [self.set,p]
+        tmp = copy.deepcopy(p)
+        vec = tmp.max()
+        for i in range(tmp.dim):
+            vec[i]=-vec[i]
+        tmp.translate_by(vec)
+        for i in range(tmp.dim):
+            tmp.normalize_coo(i)
+        append = True
+        for pp in self.set:
+            if pp.__eq__(tmp):
+                append = False
+        if append:
+            self.set.append(tmp)
 
     def size(self):
         return len(self.set)
