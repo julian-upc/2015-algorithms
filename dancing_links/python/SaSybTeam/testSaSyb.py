826b5f0da5c8e8d3f85f374de77d5a0462bf709a
Sybille Meyer <Sybille.Meyer.Berlin@gmail.com>
2015-04-24 18:59:30 +0200

    Add normalize functions, translate_by and idea for hash-code

--- dancing_links/python/SaSybTeam/testSaSyb.py
+++ dancing_links/python/SaSybTeam/testSaSyb.py
@@ -6,8 +6,11 @@ class TestSaSyb(unittest.TestCase):
     def test_SaSyb(self):
         pentomino = pentominos.F()
         print(pentomino.representation())
-        testSet = pentominos.TileSet([pentominos.I(),pentominos.X()])
-        print(testSet.representation())
+        pentomino.translate_by([2,1])
+        print(pentomino.representation())
+        #testSet = pentominos.TileSet(pentomino)
+        #print(testSet.representation())
+        print(pentomino.__eq__(pentominos.I()))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestSaSyb)
 
