f3e46198f3bb66f4b628dc8e6f7f6d913f5de325
Sybille Meyer <Sybille.Meyer.Berlin@gmail.com>
2015-05-04 14:24:21 +0200

    Pentomino finally running, adding the given files for incidence matrices

--- dancing_links/python/SaSybTeam/pentominos.py
+++ dancing_links/python/SaSybTeam/pentominos.py
@@ -1,5 +1,7 @@
 import copy
 
+max_coord = 999
+
 class Pentomino(object):
     def __init__(self, name, coos):
         self.name = name
@@ -7,7 +9,6 @@ class Pentomino(object):
         self.dim = len(coos[0])
         return self
     
-   
     def normalize_coo(self, coo):
         """ moves the given self straight to the given coo-axis.
             returns an axis touching self, with only positiv coo-coordinates
@@ -94,30 +95,38 @@ class Pentomino(object):
         else:
             print("turn90 is only defined in dimension 2")  
 
-    #the function min returns the minimum coordinate in each axis
     def min(self):
-        lower_coos = [999]*self.dim
+        """ finds the minimum coordinate in each axis,
+            these are representated as a "lower-bounding-box"
+            these lower-bounding-box is initialized with the maximal coordinates max_coord
+        """
+        lower_coos = [max_coord]*self.dim
         for i in range(self.dim):
             for c in self.coos:
                 if c[i] < lower_coos[i]:
                     lower_coos[i] = c[i]
         return lower_coos
     
-    #the function max returns the maximum coordinate in each axis
     def max(self):
-        upper_coos = [0]*self.dim
+        """ finds the maximum coordinate in each axis,
+            these are representated as an "upper-bounding-box"
+            these lupper-bounding-box is initialized with the maximal coordinates max_coord
+        """
+        upper_coos = [(-1)*max_coord]*self.dim
         for i in range(self.dim):
             for c in self.coos:
                 if c[i] > upper_coos[i]:
                     upper_coos[i] = c[i]               
         return upper_coos
-    
-    # our hash has the form "dim | coordinate of normalized pentomino | translate vector".
-    # translate vector is bounded by [0:999,0:999].
-    # example:  the pentomino: [[12,4],[13,3],[13,4],[13,5],[14,5]]
-    #           is F = [[0,1],[1,0],[1,1],[1,2],[2,2]] translated by [12,3]
-    #           hash is: 2|0110111222|012003 (without "|" of course)
+
     def __hash__(self):
+        """ gives back an integer which represents the given pentomino in an unique way.
+            our hash has the form "dim | coordinate of normalized pentomino | translate vector".
+            translate vector is bounded by [0:999,0:999].
+            example:  the pentomino: [[12,4],[13,3],[13,4],[13,5],[14,5]]
+                      is F = [[0,1],[1,0],[1,1],[1,2],[2,2]] translated by [12,3]
+                      hash is: 2|0110111222|012003 (without "|" of course)
+        """
         #save the origin position of self and normalize it
         lower_coos = self.min()
         self.normalize()
@@ -135,12 +144,17 @@ class Pentomino(object):
         return hash
 
     def __eq__(self, other):
+        """ checks the equality of the data from "self" and "other"
+        """
         return self.__hash__() == other.__hash__()
 
     def representation(self):
+        """ Gives back a string representation of the given self
+        """
         return "[" + self.name + ":" + str(self.coos) + "]"
 
-
+""" The following classes implements all kinds of pentominos in normalized coordinates
+"""
 class F(Pentomino):
     def __init__(self):
         Pentomino.__init__(self, "F", [[0,1],[1,0],[1,1],[1,2],[2,2]])
@@ -189,24 +203,33 @@ class Z(Pentomino):
     def __init__(self):
         Pentomino.__init__(self, "Z", [[0,2],[1,0],[1,1],[1,2],[2,0]])
 
-# this function creates a TileSet of all normalized representatives of the given pentomino p
 def fixed_pentominos_of(p):
+    """ adds all possible representations of the given pentomino (turned and flipped) to self
+    """
     allReps = TileSet()
     for i in range(4):
         allReps.add(p.turn90())
     allReps.add(p.flip(0))
     for i in range(3):
         allReps.add(p.turn90())
+    return allReps
 
 def all_fixed_pentominos():
-    all_pSet = Tileset()
+    """ gives back all possible representations of all possible pentominos 
+    """
+    all_pSet = TileSet()
     pento_list = all_pentominos()
     for p in pento_list:
-        all_pSet.add(fixed_pentominos(p))
+        all_pSet.add_TileSet(fixed_pentominos_of(p))
+    return all_pSet
 
 def all_pentominos():
+    """ gives back all possible pentominos
+    """
     return [F(), I(), L(), P(), N(), T(), U(), V(), W(), X(), Y(), Z()]
 
+""" 
+"""
 class TileSet(object):
     
     def __init__(self, plist=[]):
@@ -216,25 +239,27 @@ class TileSet(object):
 
     def __iter__(self):
         return iter(self.set)
-
-    # this function adds a pentomino to this TileSet    
+ 
     def add(self, p):
+    """ adds the given pentomino to self
+    """
         if p not in self.set:
             self.set.add(copy.deepcopy(p))
-        return self.set
     
-    # this function adds a TileSet to this TileSet
     def add_TileSet(self, tileSet):
-        self.set.add(copy.deepcopy(tileSet)
-	return self.set
+    """ adds the given tileSet to self
+    """
+        for p in tileSet:
+            if p not in self.set:
+                self.add(p)
+        return self
                      
-    def size(self):
-	return len(self.set)
-
     def size(self):
         return len(self.set)
                      
     def representation(self):
+    """ gives back a representation for tileSet
+    """
         rep = "["
         i = 0
         for p in self.set:
