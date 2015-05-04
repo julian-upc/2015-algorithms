fa745c75678efa19b7d3b0cd12faab5eb5faf827
anneSophieHimmel <anne-himmel@gmx.de>
2015-05-04 21:16:45 +0200

    added files

---
+++ dancing_links/python/AH_FS/incidence_matrix.py
@@ -0,0 +1,113 @@
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        pass
+    return False
+
+
+class IncidenceCell(object):
+    def __init__(self, left, right, up, down, listHeader, name):
+        self.left = left
+        self.right = right
+        self.up = up
+        self.down = down
+        self.listHeader = listHeader
+        self.name = name
+
+    def representation(self):
+        rep = ["c", self.name]
+        for c in [self.left, self.right, self.up, self.down]:
+            rep.append(c.name)
+        return rep
+
+        
+class ColumnObject(IncidenceCell):
+    def __init__(self, left, right, up, down, name):
+        IncidenceCell.__init__(self, left, right, up, down, self, name)
+        self.size = 0
+
+    def representation(self):
+        hrep = ["h(" + str(self.size) + ")", self.name]
+        for c in [self.left, self.right, self.up, self.down]:
+            hrep.append(c.name)
+        rep = [[hrep]]
+
+        currentCell = self.down
+        while currentCell is not self:
+            rep[0].append(currentCell.representation())
+            currentCell = currentCell.down
+        
+        return rep
+
+
+class IncidenceMatrix(object):
+    def __init__(self, names):
+        self.h = ColumnObject(None, None, None, None, "root")
+        self.h.left = self.h.right = self.h.up = self.h.down = self.h
+
+        currentColumnObject = self.h
+        self.columnObjectOfName = dict()
+        self.columnObjectOfName["root"] = self.h
+
+        self.indexOfPiecePlacement = dict()
+        for n in names:
+            self.indexOfPiecePlacement[n] = 0
+            self.insertColumnObject(currentColumnObject, self.h, n)
+            currentColumnObject = currentColumnObject.right
+            self.columnObjectOfName[n] = currentColumnObject
+
+        self.rows = 0
+
+    def representation(self):
+        currentColumnObject = self.h
+
+        rep = currentColumnObject.representation()
+        currentColumnObject = currentColumnObject.right
+
+        while currentColumnObject.name is not "root":
+            rep += currentColumnObject.representation()
+            currentColumnObject = currentColumnObject.right
+            
+        return rep
+
+    def rowRepresentation(self):
+        rowRep = []
+        currentColumnObject = self.h.right
+        while currentColumnObject.name is not "root" and not is_number(currentColumnObject.name):
+            head_elt = currentColumnObject.down
+            while head_elt is not currentColumnObject:
+                row = [ head_elt.name ]
+                current_elt = head_elt.right
+                while current_elt is not head_elt:
+                    row.append(current_elt.listHeader.name)
+                    current_elt = current_elt.right
+                rowRep.append(row)
+                head_elt = head_elt.down
+            currentColumnObject = currentColumnObject.right
+        return rowRep
+
+    def insertColumnObject(self, left, right, name):
+        cur = ColumnObject(left, right, None, None, name);
+        cur.up = cur.down = cur
+        left.right = right.left = cur
+
+    def appendRow(self, tileName, placement):
+        """ a placement is a list of coordinates that indicates which squares the piece named tileName covers"""
+        tileColHeader = self.columnObjectOfName[tileName]
+        rowCell = IncidenceCell(None, None, tileColHeader.up, tileColHeader, name)
+        rowCell.left = rowCell.right = rowCell.up.down = rowCell.down.up = rowCell
+        
+        cell = rowCell
+        for n in placement:
+            header = self.columnObjectOfName[n]
+            newCell = IncidenceCell(cell, rowCell, header.up, header, header, n)
+            cell.right = rowCell.left = header.up.down = header.up = newCell
+            cell = newCell
+
+    def coverColumn(self, c):
+        pass
+
+    def uncoverColumn(self, c):
+        pass
