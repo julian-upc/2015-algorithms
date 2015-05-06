995c69c3f813cb3652c8f0d84f56b00beb120d44
ay4me <m.hatzel@campus.tu-berlin.de>
2015-05-06 12:37:31 +0200

    further steps for final scott problem

--- dancing_links/python/ay4me/scott.py
+++ dancing_links/python/ay4me/scott.py
@@ -3,12 +3,28 @@ import incidence_matrix
 
 class Problem(object):
 	"""The problem gets a TileSet of pentominos to work with. The range of the field to cover. 
-	A taboolist of forbidden coordinates and a number how often each single tile is allowed to be used.
+	A function legal() that tests whether a set of coordinates is legal and a number how often each single tile is allowed to be used.
 	This number is -1 if the tiles may be used arbitrary often."""
 	def __init__(self,WorkingSet,FRange,legal,usgNr):
-		self.IncMatrix = createMatrix(WorkingSet,FRange,taboo)
+		self.WorkingSet = WorkingSet
+		self.tiles = []
+		for p in self.WorkingSet:
+			self.tiles.extend(pentominos.fixed_pentominos_of(p))
+		self.tileNames = {}
+		for t in self.tiles:
+			n = t.name + "1"
+			while n in self.tileNames.values():
+				x = n[-1]
+				n = n[:-1]
+				num = int(x)+1
+				n += str(num)
+			self.tileNames[t] = n
+		self.FRange = FRange
 		self.usgNr = usgNr
-		return self
+		self.legal = legal
+		self.IncMatrix = self.createMatrix()
+		#self.usgNr = usgNr
+		#print "created new problem"
 
 
 	def AllowTabooCoos(c,taboo):
@@ -17,13 +33,46 @@ class Problem(object):
 		else:
 			return False
 
-	def createMatrix(WorkingSet,FRange,taboo):
+	def createMatrix(self):
+		"""creates the columns for the matrix"""
 		#names = [p.name for p in WorkingSet]
+		
 		names = []
-		for p in WorkingSet:
-			names.append(p.name)
-		for i in range(FRange):
-			for j in range(FRange):
-				if legal([i,j]):
+		for p in self.tiles:
+			names.append(self.tileNames[p])
+		for i in range(self.FRange):
+			for j in range(self.FRange):
+				if self.legal([i,j]):
 					names.append(str(i)+str(j))
 		return incidence_matrix.IncidenceMatrix(names)
+
+	def completeMatrix(self):
+		"""creates all rows"""
+		for p in self.WorkingSet:
+			tiles = pentominos.fixed_pentominos_of(p)
+			#print tiles.representation()
+			for t in tiles:
+				for i in range(self.FRange):
+					for j in range(self.FRange):
+						#print "test coordinates " + str(i) + " and "+str(j)
+						t.translate_by([i,j])
+						l = True
+						for x in t.coos:
+							if not self.legal(x):
+								l = False
+								break
+						if l:
+							self.IncMatrix.appendRow(self.tileNames[t],self.placement(t))
+						t.translate_by([-i,-j])
+
+
+	def placement(self,tile):
+		coos = tile.coos
+		placement = []
+		for c in coos:
+			placement.append(str(c[0])+str(c[1]))
+		return placement
+
+	def solve():
+		
+		pass
--- dancing_links/python/ay4me/test_scott.py
+++ dancing_links/python/ay4me/test_scott.py
@@ -11,22 +11,38 @@ class TestScottProblem(unittest.TestCase):
 	def testSetup(self):
 		TSet = pentominos.TileSet([pentominos.F(),pentominos.I(),pentominos.L(),pentominos.P(),pentominos.N(),pentominos.T(),pentominos.U(),pentominos.V(),pentominos.W(),pentominos.X(),pentominos.Y(),pentominos.Z()])
 		names = [p.name for p in TSet]
-		self.assertEqual(names,["F", "I", "L", "P", "N", "T", "U", "V", "W", "X", "Y", "Z"])
-		prob = scott.Problem(TSet,8,legal,1)
-		IM = prob.IncMatrix
-        prob.IncMatrix.appendRow("I", ["00", "01", "02", "03", "04"])
-        prob.IncMatrix.appendRow("I", ["01", "02", "03", "04", "05"])
-        rep = [prob.IncMatrix.columnObjectOfName[i].representation() for i in ["I", "00", "01", "02", "03", "04"]]
-        self.assertEqual([[[['h(2)', 'I', 'F', 'L', 'I[1]', 'I[0]'], ['c', 'I[0]', 'I04', 'I00', 'I', 'I[1]'], ['c', 'I[1]', 'I05', 'I01', 'I[0]', 'I']]], [[['h(1)', '00', 'Z', '01', 'I00', 'I00'], ['c', 'I00', 'I[0]', 'I01', '00', '00']]], [[['h(2)', '01', '00', '02', 'I01', 'I01'], ['c', 'I01', 'I00', 'I02', '01', 'I01'], ['c', 'I01', 'I[1]', 'I02', 'I01', '01']]], [[['h(2)', '02', '01', '03', 'I02', 'I02'], ['c', 'I02', 'I01', 'I03', '02', 'I02'], ['c', 'I02', 'I01', 'I03', 'I02', '02']]], [[['h(2)', '03', '02', '04', 'I03', 'I03'], ['c', 'I03', 'I02', 'I04', '03', 'I03'], ['c', 'I03', 'I02', 'I04', 'I03', '03']]], [[['h(2)', '04', '03', '05', 'I04', 'I04'], ['c', 'I04', 'I03', 'I[0]', '04', 'I04'], ['c', 'I04', 'I03', 'I05', 'I04', '04']]]], rep)
+		for n in ["F", "I", "L", "P", "N", "T", "U", "V", "W", "X", "Y", "Z"]:
+			self.assertTrue(n in names)
+		for n in names:
+			self.assertTrue(n in ["F", "I", "L", "P", "N", "T", "U", "V", "W", "X", "Y", "Z"])
+		prob = scott.Problem(TSet,8,self.legal,1)
+		# IM = copy.deepcopy(prob.IncMatrix)
+		# IM.appendRow("I", ["00", "01", "02", "03", "04"])
+		# IM.appendRow("I", ["01", "02", "03", "04", "05"])
+		# rep = [IM.columnObjectOfName[i].representation() for i in ["I", "00", "01", "02", "03", "04"]]
+		# self.assertEqual([[[['h(2)', 'I', 'root', 'F', 'I[1]', 'I[0]'], ['c', 'I[0]', 'I04', 'I00', 'I', 'I[1]'], ['c', 'I[1]', 'I05', 'I01', 'I[0]', 'I']]], 
+		# 	[[['h(1)', '00', 'V', '01', 'I00', 'I00'], ['c', 'I00', 'I[0]', 'I01', '00', '00']]], [[['h(2)', '01', '00', '02', 'I01', 'I01'], ['c', 'I01', 'I00', 'I02', '01', 'I01'], 
+		# 	['c', 'I01', 'I[1]', 'I02', 'I01', '01']]], [[['h(2)', '02', '01', '03', 'I02', 'I02'], ['c', 'I02', 'I01', 'I03', '02', 'I02'], ['c', 'I02', 'I01', 'I03', 'I02', '02']]], 
+		# 	[[['h(2)', '03', '02', '04', 'I03', 'I03'], ['c', 'I03', 'I02', 'I04', '03', 'I03'], ['c', 'I03', 'I02', 'I04', 'I03', '03']]], [[['h(2)', '04', '03', '05', 'I04', 'I04'], 
+		# 	['c', 'I04', 'I03', 'I[0]', '04', 'I04'], ['c', 'I04', 'I03', 'I05', 'I04', '04']]]], rep)
+		# #print IM.representation()
 
-	def legal(coos):
-		for p in coos:
-			if p in [[3,3],[3,4],[4,3],[4,4]] or p[0]<0 or p[0]>7 or p[1]<0 or p[1]>7:
-				return False
+	def testMatrix(self):
+		TSet = pentominos.TileSet([pentominos.F(),pentominos.I(),pentominos.L(),pentominos.P(),pentominos.N(),pentominos.T(),pentominos.U(),pentominos.V(),pentominos.W(),pentominos.X(),pentominos.Y(),pentominos.Z()])
+		names = [p.name for p in TSet]
+		prob = scott.Problem(TSet,8,self.legal,1)
+		str1 = prob.IncMatrix.representation()
+		prob.completeMatrix()
+		str2 = prob.IncMatrix.representation()
+		self.assertFalse(str1 == str2)
+
+	def legal(self,coos):
+		if coos in [[3,3],[3,4],[4,3],[4,4]] or coos[0]<0 or coos[0]>7 or coos[1]<0 or coos[1]>7:
+			return False
 		return True
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestIncidenceMatrixMethods)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestScottProblem)
 
 if __name__ == '__main__':
     unittest.main()
