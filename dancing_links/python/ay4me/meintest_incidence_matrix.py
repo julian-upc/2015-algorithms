2a69ed84986934e5864aea019de7e35201323b2c
ay4me <m.hatzel@campus.tu-berlin.de>
2015-05-07 12:57:03 +0200

    found endless-loop, but there is still some fault

---
+++ dancing_links/python/ay4me/meintest_incidence_matrix.py
@@ -0,0 +1,8 @@
+import incidence_matrix
+import pentominos
+import examples
+import copy
+
+
+I = incidence_matrix.IncidenceMatrix(["0", "1", "2"])
+print I.representation()
--- dancing_links/python/ay4me/scott.py
+++ dancing_links/python/ay4me/scott.py
@@ -23,7 +23,9 @@ class Problem(object):
 		self.FRange = FRange
 		self.usgNr = usgNr
 		self.legal = legal
-		self.IncMatrix = self.createMatrix()
+		self.IncMatrixX12 = self.createMatrix()
+		self.IncMatrixX13 = self.createMatrix()
+		self.IncMatrixX22 = self.createMatrix()
 		#self.usgNr = usgNr
 		#print "created new problem"
 
@@ -49,19 +51,24 @@ class Problem(object):
 			tiles = pentominos.fixed_pentominos_of(p)
 			#print tiles.representation()
 			for t in tiles:
-				for i in range(self.FRange):
-					for j in range(self.FRange):
-						#print "test coordinates " + str(i) + " and "+str(j)
-						t.translate_by([i,j])
-						l = True
-						for x in t.coos:
-							if not self.legal(x):
-								l = False
-								break
-						if l:
-							# self.IncMatrix.appendRow(self.tileNames[t],self.placement(t))
-							self.IncMatrix.appendRow(t.name,self.placement(t))
-						t.translate_by([-i,-j])
+				if t.name == "X":
+					self.IncMatrix.appendRow("X", ["02", "11", "12", "13", "22"])#12
+					self.IncMatrix.appendRow("X", ["03", "12", "13", "14", "23"])#13
+					self.IncMatrix.appendRow("X", ["12", "21", "22", "23", "32"])#22 (21 and 32 unique to this case)
+				else:
+					for i in range(self.FRange):
+						for j in range(self.FRange):
+							#print "test coordinates " + str(i) + " and "+str(j)
+							t.translate_by([i,j])
+							l = True
+							for x in t.coos:
+								if not self.legal(x):
+									l = False
+									break
+							if l:
+								# self.IncMatrix.appendRow(self.tileNames[t],self.placement(t))
+								self.IncMatrix.appendRow(t.name,self.placement(t))
+							t.translate_by([-i,-j])
 
 
 	def placement(self,tile):
@@ -73,12 +80,59 @@ class Problem(object):
 
 	def solve(self):
 		O = {}
-		O = self.search(0,O)
-		print "solution found:" + O
+		# #find X column
+		# currentColumn = h
+		# while currentColumn.name != "X":
+		# 	currentColumn = currentColumn.right
+		# self.IncMatrix.coverColumn(currentColumn) #cover X column
+		# for pos in [12,13,22]:
+		# 	currentRow = currentColumn.down
+		# 	while not has(pos,currentRow):#find row that has the position
+		# 		currentRow = currentRow.down
+		# 	O[0] = currentRow #add this row to solution
+		# 	currentColumn = currentRow.right
+		# 	while currentColumn is not currentRow:
+		# 		self.IncMatrix.coverColumn(currentColumn.listHeader)
+		# 		currentColumn = currentColumn.right
+		# 	if pos == 22:
+		# 		#remove flipped P
+		# 	sol = self.search(1,O)
+		# 	if sol != {}:
+		# 	print "solution found:" + sol
+		# 	currentRow = O[0]
+		# 	c = currentRow.listHeader #necessary?
+		# 	currentColumn = currentRow.left #necessary?
+		# 	while currentColumn is not currentRow:
+		# 		self.IncMatrix.uncoverColumn(currentColumn.listHeader)
+		# 		currentColumn = currentColumn.left
+		sol = self.search(0,O)
+		if sol != {}:
+			print "solution found:" + sol
+		else:
+			print "no solution"
+		return sol
+
+	def has(self,value,currentRow):
+		isin = False
+		row = currentRow.right
+		while row is not currentRow:
+			if row.listHeader.name == str(value):
+				isin = True
+			row = row.right
+		return isin
 
 	def search(self,k,O):
 		if self.IncMatrix.h.right is self.IncMatrix.h:
-			return O
+			#return solution as string
+			solution = ""
+			for i in range(k):
+				currentRow = O[i]
+				solution += currentRow.listHeader.name
+				currentColumn = currentRow.right
+				while currentColumn is not currentRow:
+					solution += currentColumn.listHeader.name
+					currentColumn = currentColumn.right
+			return solution
 		else:
 			c = self.IncMatrix.h
 			s = sys.maxint
@@ -87,21 +141,40 @@ class Problem(object):
 				if j.size < s:
 					c = j
 					s = j.size
+				j = j.right
 			self.IncMatrix.coverColumn(c)
 			currentRow = c.down #r
-        	while currentRow is not c:
-        		O[k] = currentRow
-        		currentColumn = currentRow.right
-        		while currentColumn is not r:
-        			self.IncMatrix.coverColumn(currentColumn.listHeader)
-        			currentColumn = currentColumn.right
-        		self.search(k+1,O)
-        		currentRow = O[k]
-        		c = currentRow.listHeader #necessary?
-        		currentColumn = currentRow.left #necessary?
-        		while currentColumn is not r:
-        			self.IncMatrix.uncoverColumn(currentColumn.listHeader)
-        			currentColumn = currentColumn.left
-        		currentRow = currentRow.down
-        	self.IncMatrix.uncoverColumn(c)
-        	return {}
+			while currentRow is not c:
+				O[k] = currentRow
+				currentColumn = currentRow.right
+				while currentColumn is not currentRow:
+					self.IncMatrix.coverColumn(currentColumn.listHeader)
+					currentColumn = currentColumn.right
+				if c.name == "X" and self.has(21,currentRow):
+					#remove all flipped P's
+					#self.removeFlippedP(self)
+					pass
+				self.search(k+1,O)
+				currentRow = O[k]
+				c = currentRow.listHeader #necessary?
+				if c.name == "X" and self.has(21,currentRow):
+					#remove all flipped P's
+					#self.restoreFlippedP(self)
+					pass
+				currentColumn = currentRow.left #necessary?
+				while currentColumn is not currentRow:
+					self.IncMatrix.uncoverColumn(currentColumn.listHeader)
+					currentColumn = currentColumn.left
+				currentRow = currentRow.down
+			self.IncMatrix.uncoverColumn(c)
+			return {}
+
+	def removeFlippedP(self):
+		#find P column
+		currentColumn = h
+		while currentColumn.name != "P":
+			currentColumn = currentColumn.right
+
+
+	def restoreFlippedP(self):
+		pass
