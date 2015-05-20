d94ad53df2dd839e0d5d10ea790b92c7968261ca
SaWein <SaWein@users.noreply.github.com>
2015-05-20 19:56:38 +0200

    Restored dancing_links/python/incidence_matrix.py

--- dancing_links/python/incidence_matrix.py
+++ dancing_links/python/incidence_matrix.py
@@ -89,15 +89,8 @@ class IncidenceMatrix(object):
         return rowRep
 
     def insertColumnObject(self, left, right, name):
-        """ insert a column header object into the circular linked list 
-            that contains the "root" node """
-        newColumn = ColumnObject(left,right,None,None,name)
-        newColumn.listHeader = newColumn
-        newColumn.up = newColumn
-        newColumn.down = newColumn
-        newColumn.left.right = newColumn
-        newColumn.right.left = newColumn
-        return self
+        """ insert a column header object into the circular linked list that contains the "root" node """
+        pass
 
     def appendRow(self, tileName, placement):
         """ 
@@ -108,92 +101,12 @@ class IncidenceMatrix(object):
         These must be assembled into a circularly linked list, and each cell must be inserted into the 
         circular linked list of its corresponding column.
         """
-        #try to insert a row on given columnOfName[tileName], 
-        # this could fail, if there is no such ColumnObject 
-        try:
-        # Add a cell (corresponding to pentomino) at the end of the 
-        # matching pentomino type Column
-            currentColumn = self.columnObjectOfName[tileName]
-            # The name is e.g. I[1] for the 2nd Pentomino of type I.
-            pentoCellName = tileName + '[' + str(currentColumn.size) + ']'
-            newPentoCell = IncidenceCell(None,None,currentColumn.up,
-                                currentColumn, currentColumn, pentoCellName)
-            # rearrange the links of the circularly list and upgrade the size            
-            currentColumn.up.down = newPentoCell            
-            currentColumn.up = newPentoCell
-            self.columnObjectOfName[tileName].size += 1
-        # add 5 cells (corresponding to placement of pentomino) at the 
-        # end of the matching Coordinate Columns
-            placement.sort()
-            leftNeighbour = newPentoCell
-            for co in placement:
-                currentColumn = self.columnObjectOfName[co]
-                # the name us e.g. I00, if this I pentomino covers the place 00
-                cooCellName = tileName + currentColumn.name 
-                newCooCell = IncidenceCell(leftNeighbour,None,currentColumn.up,
-                                currentColumn,currentColumn,cooCellName)
-                # rearrange the links of the current circular lists and 
-                # upgrade the size                 
-                leftNeighbour.right = newCooCell
-                currentColumn.up.down = newCooCell            
-                currentColumn.up = newCooCell
-                leftNeighbour = newCooCell
-                currentColumn.size += 1
-            # links the beginning of this row (the pentominoCell) with the end
-            # of this row (last placementCell) --> circular list
-            newPentoCell.left = leftNeighbour
-            leftNeighbour.right = newPentoCell
-            return self
-        except:
-            print('There accured a problem appending a row in column'+tileName)
-    
-    # The operation of covering column c removes c from the header list and 
-    # removes all rows in c's own list from the other column lists they are in
+        pass
+
     def coverColumn(self, c):
-        """ implement and document the algorithm in Knuth's paper.""" 
-        # nehme den Header der Spalte c        
-        curr = c.listHeader
-        # setze fuer alle Objekte in c den Linken und rechten Zeiger um
-        c.right.left = c.left
-        c.left.right = c.right
-        # merke dir das erste Element unter dem Header
-        d = curr.down
-        # iteriere ueber die Spalte von oben nach unten
-        while d is not curr:
-            # merke dir das naechste Element in der Zeile
-            r = d.right
-            # iteriere ueber die Zeile von links nach rechts
-            while r is not d:
-                # setze fuer alle Elemente in dieser Zeile die Zeiger fuer up & down um
-                r.down.up = r.up
-                r.up.down = r.down
-                # gehe eins weiter die Zeile entlang
-                d.right = r.right
-            # gehe ein Element weiter die Spalte entlang
-            d = d.down
-            # verkuerze die Laenge der Spalte um 1, da eine Zeile abgearbeitet wurde
-            c.size = c.size-1 # koennte falsch sein
-        return self
-                      
+        """ implement and document the algorithm in Knuth's paper. """
+        pass
+
     def uncoverColumn(self, c):
-        """ implement and document the algorithm in Knuth's paper.""" 
-        curr = c.listHeader
-        i = curr.up
-        # iteriere ueber die Spalte von unten nach oben
-        while i is not curr:
-            j = i.left
-            # iteriere ueber die Zeile links nach rechts
-            while j is not i:
-                c.size = c.size+1 # koennte falsch sein
-                # 
-                j.down.up = j
-                j.up.down = j
-                # iteriere in der Zeile eins weiter
-                j = j.left
-            # dies mit der oberen Schleife tauschen!??!? Nochmal in Ruhe durchdenken    
-            curr.right.left = curr
-            curr.left.right = curr
-            curr = curr.up
-        return self
-        
-            
+        """ implement and document the algorithm in Knuth's paper. """
+        pass
