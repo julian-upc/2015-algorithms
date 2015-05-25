a6b1d361e7966d4d33f013ad51415824697edf8e
ay4me <m.hatzel@campus.tu-berlin.de>
2015-05-25 16:51:57 +0200

    created own subdirectory

---
+++ coxeter_group_orbits/ay4me/program.cc
@@ -0,0 +1,35 @@
+#include __ORBIT_H_
+
+
+//read input rom file
+//give input to generateNormalVectors
+//orbit(generators, v)
+//write size of orbit
+//write orbit if flag
+
+?? read(file)
+{
+   //read coxeter(type, n) from file
+   //read vector from file
+   //throw exeption when not a finite coxeter
+   //throw exeption when vector not of size n
+}
+
+GeneratorList generateNormalVectors(coxetertype, dim)
+{
+
+}
+
+Orbit orbit(const GeneratorList& generators, const VectorType& v)
+{
+   return std::set<VectorType>();
+}
+
+
+#endif // __ORBIT_H_
+
+// Local Variables:
+// mode:C++
+// c-basic-offset:3
+// indent-tabs-mode:nil
+// End:
