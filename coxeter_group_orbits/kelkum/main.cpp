9118e54cf4690d3d2e0db6602fdce765c09097ed
1nclusion <Frans.Kummer@gmail.com>
2015-06-06 20:23:31 +0200

    added folder in coxeter folder and started writing the program, still bugged though

---
+++ coxeter_group_orbits/kelkum/main.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include "orbit.h"
+
+int main() {
+    std::string coxeterDiagram = "";
+    VectorType inputPoint; //see orbit.h for what this type exactly is
+    Orbit pointOrbit;  //same
+    double coord; 
+    unsigned int orbitLength;
+    bool showOrbit;  
+    bool renderOrbit;
+    int e;
+
+    try {
+        //input
+        //assumes file is in the right format eg "I5\n1.0,1.0,1.0,1.0,1.0", no checking if input is sensable and no real parsing over ambiguities is done
+        std::fstream file ("input.txt"); 
+        std::stringstream tmpss;
+        std::string tmpLine;
+
+        if (file.is_open()) {
+            std::getline(file, tmpLine);
+            coxeterDiagram = tmpLine;
+
+            while(std::getline(file, tmpLine, ',')) {
+                tmpss << tmpLine;
+                tmpss >> coord;
+                inputPoint.push_back(coord);
+            }
+            file.close();
+        } else {
+            throw InputError;
+        }
+
+        //processing
+        //function is in orbit.h
+        pointOrbit = processing(coxeterDiagram, inputPoint);
+        orbitLength = pointOrbit.size(); 
+    } catch(errorTypes& e) {
+        std::cerr << "Errorcode " << e << '\n';
+    }
+
+        //output
+        std::cout << "Orbit size: " << orbitLength << "\n show orbit? Y=1, N=0: "; 
+        std::cin >> showOrbit;
+        if(showOrbit) {
+            std::cout.precision(5);
+            std::set<VectorType>::iterator iter; 
+            for (iter=pointOrbit.begin(); iter!=pointOrbit.end(); iter++) {
+                std::cout << "( ";
+                for (int j=0; j<(*iter).size()-1; j++) {
+                    std::cout << (*iter)[j] << ',';
+                }
+                std::cout << (*iter).size()-1 << ")\n";
+            }
+        if (inputPoint.size() == 3 || inputPoint.size() == 2) {
+            std::cout << "Render Orbit? Y=1, N=0: ";
+            std::cin >> renderOrbit; 
+        }
+        if (renderOrbit) {
+            //..
+        }
+    return 0;
+}
---
+++ coxeter_group_orbits/kelkum/orbit.h
@@ -0,0 +1,176 @@
+/* Copyright (c) 2015
+   Julian Pfeifle
+   julian.pfeifle@upc.edu
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 3, or (at your option) any
+   later version: http://www.gnu.org/licenses/gpl.txt.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+--------------------------------------------------------------------------------
+*/
+
+#ifndef __ORBIT_H_
+#define __ORBIT_H_
+
+#include <vector>
+#include <set>
+#include <sstream>
+#include <set>
+
+
+template<typename T> //only numeric types please
+class ImperciseVector: public std::vector<T> {
+    public:
+        ImperciseVector() {}
+        bool operator~ (const ImperciseVector& v) { //does not work yet
+           T l1sq=0.0, l2sq=0.0, l3sq=0.0;
+
+           if ((*this).length() != v.length() || (*this).length==0 || v.length()==0) return false; 
+           for (int j = 0; j<(*this).length; j++) {
+               l1sq += (*this)[j]*(*this)[j];
+               l2sq += v[j]*v[j];
+               l3sq+= ((*this)[j]-v[j])*((*this)[j]-v[j]);
+           }
+           //some estimate such that length of their  their difference (relative to their size) is small enough
+           if (l3sq/(l1sq*l2sq) <= 0.00001) return true; 
+           return false;
+        }
+};
+
+typedef ImperciseVector<double> VectorType;
+typedef std::vector<VectorType> GeneratorList;
+typedef std::set<VectorType> Orbit;
+enum errorTypes {NotImplemented, DimensionError, InputError, DotProdNotDefined};
+
+void simple_roots(char type, int dim, GeneratorList& normals); //forward declaration of function
+Orbit genorbit(const GeneratorList& generators, const VectorType& v); //forward declaration of function
+
+Orbit processing(std::string& coxeterDiagram, VectorType& inputPoint) {
+    std::stringstream ss; 
+    ss << coxeterDiagram;
+
+    char diagramType;
+    int normalNumber;
+    GeneratorList normals;
+    Orbit orbit; 
+
+    ss >> diagramType >> normalNumber; //not sure but it seems to work
+    int dimensionPoint = inputPoint.size(); 
+
+    //checking/correcting input ambiguities
+    if (dimensionPoint != normalNumber || normalNumber<=0) {
+        throw DimensionError;
+    }
+    if (diagramType == 'A' || diagramType == 'a') { //make dimensions of normals and the point in this special case the same so one gets sensable dotproducts
+        inputPoint.push_back(0.0);
+    }
+    simple_roots(diagramType, normalNumber, normals); //modifies the normals Variable to get all the normals corresponding to a certain Coxeter diagram into the normals "matrix"
+    orbit = genorbit(normals, inputPoint);
+
+    return orbit; 
+
+}
+
+void simple_roots(char type, int dim, GeneratorList& normals)
+{
+   VectorType tmpVector;
+   double tmpEntry;
+   normals.clear();
+   
+   switch(type) { 
+       case 'a': case 'A':
+           for (int vectorNr = 0; vectorNr<dim; vectorNr++) {
+               tmpVector.clear();
+               for (int vectorEntry = 0; vectorEntry<dim+1; vectorEntry++) {
+                   if (vectorNr==vectorEntry) tmpEntry = 1.0; 
+                   else if (vectorNr==vectorEntry-1) tmpEntry = -1.0;
+                   else tmpEntry = 0.0;
+                   tmpVector.push_back(tmpEntry);
+               }
+               normals.push_back(tmpVector);
+           }
+           break;
+
+       case 'b': case 'B':
+          for (int vectorNr = 0; vectorNr<dim; vectorNr++) {
+               tmpVector.clear();
+               for (int vectorEntry = 0; vectorEntry<dim; vectorEntry++) {
+                   if (vectorNr==vectorEntry) tmpEntry = 1.0; 
+                   else if (vectorNr==vectorEntry-1) tmpEntry = -1.0;
+                   else tmpEntry = 0.0;
+                   tmpVector.push_back(tmpEntry);
+               }
+               normals.push_back(tmpVector);
+           }
+           break;
+
+           //other diagrams need to be implemented
+
+       default:
+          throw NotImplemented;
+   }
+}
+
+double dotprod(const VectorType& v1, const VectorType& v2) {
+    if (v1.size()!=v2.size()) {
+        throw DotProdNotDefined;
+        return 0.0;
+    }
+    int d = 0;
+    for (int i = 0; i<v1.size(); i++) {
+        d+=v1[i]*v2[i];
+    }
+    return d;
+}
+
+//requires a VectorType as input that it can write the result of the reflection to, to try to avoid copying and reassigning as this is the function used the most often
+void reflect(const VectorType& normal, const VectorType& point, VectorType& result) {
+    VectorType result=point;
+    double pointDotNormal = dotprod(point, normal);
+    double normalDotNormal = dotprod(normal, normal);
+    for (int i = 0; i<point.size(); i++) {
+        result[i]-=2*(pointDotNormal/normalDotNormal)*normal[i];
+    }
+}
+
+Orbit genorbit(const GeneratorList& generators, const VectorType& v) {
+    std::set<VectorType> pointOrbit;
+    pointOrbit.insert(v);
+    std::set<VectorType>::iterator it;
+
+    VectorType tmpPoint;
+    GeneratorList buffer1, buffer2; 
+    buffer2.push_back(v);
+    GeneratorList* writingBufferNewPoints = &buffer1;
+    GeneratorList* readingBufferNewPoints = &buffer2;
+
+    int compositionLength=100;
+
+    for (int i = 0; i<compositionLength; i++) {
+        for (int j = 0; j<(*readingBufferNewPoints).size(); j++) { //iterate through all new points
+            for (int k=0; k<generators.size(); k++) { //reflect each individual new point in all planes
+                tmpPoint.clear(); 
+                reflect(generators[k], (*readingBufferNewPoints)[j], tmpPoint);
+                //now check if new creation is already in pointOrbit, if not add it to the pointOrbit and to writingBufferNewPoints that will be read from in the next iteration of i
+                for (it=pointOrbit.begin(); it!=pointOrbit.end(); it++) {
+                    if (tmpVector~(*it2)) break; //approximate comparison
+                    pointOrbit.insert(tmpVector);
+                    (*writingBufferNewPoints).push_back(tmpVector);
+                }
+            }
+        }
+        (*readingBufferNewPoints).clear(); 
+        std::swap(writingBufferNewPoints, readingBufferNewPoints);
+    }
+
+
+    return pointOrbit;
+}
+
+
+#endif // __ORBIT_H_
