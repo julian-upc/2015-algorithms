7403f229cf2d6b76f3d8e8a8d2be18da8aea0c73
ay4me <m.hatzel@campus.tu-berlin.de>
2015-06-07 23:37:07 +0200

    changed vectors from int to double, with relative error for comparisons to compensate rounding errors

--- coxeter_group_orbits/ay4me/orbit.h
+++ coxeter_group_orbits/ay4me/orbit.h
@@ -24,6 +24,7 @@
 #include <iostream>
 #include <string>
 #include <numeric>
+#include <cmath>
 #include <boost/lexical_cast.hpp>
 
 class NotImplementedException : public std::exception {};
@@ -38,10 +39,101 @@ public:
   InvalidGeneratorException(const std::string& s):std::logic_error(s) {};
 };
 
-typedef int NumberType;  // this probably isn't going to work
+const static double epsilon = 0.00001;
+
+// template<typename T>
+// class ImpreciseVector : public std::vector<T>
+// {
+//   public:
+//     ImpreciseVector(){}
+//     //ImpreciseVector(const std::size_t& arg) : std::vector<T>(arg) {}
+
+//     double relativeError(const ImpreciseVector<T>& x, const ImpreciseVector<T>& y)
+//     {
+//       ImpreciseVector diff(x.size());
+//       for (int i=0;i < x.size();++i)
+//       {
+//         diff[i] = x[i]-y[i];
+//       }
+//       double maxDiff = 0.0;
+//       double maxVal = 0.0;
+//       for (int i=0;i < x.size();++i)
+//       {
+//         if (diff[i] > maxDiff)
+//         {
+//           maxDiff = diff[i];
+//         }
+//         if (x[i] > maxVal)
+//         {
+//           maxVal = x[i];
+//         }
+//       }
+//       return maxDiff/maxVal;
+//     }
+
+//     friend bool operator <(const ImpreciseVector<T>& x, const ImpreciseVector<T>& y)
+//     {
+//       if (relativeError(x,y) < epsilon)
+//       {
+//         return false;
+//       }
+//       return static_cast<std::vector<T>>(x) < static_cast<std::vector<T>>(y);
+//     }
+// };
+
+template<typename T>
+T relativeError(const std::vector<T>& x, const std::vector<T>& y)
+{
+  if (x.size() != y.size())
+  {
+    throw std::logic_error("Comparison between vectors of different size.");
+  }
+  std::vector<T> diff(x.size());
+  for (std::size_t i=0;i < x.size();++i)
+  {
+    diff[i] = x[i]-y[i];
+  }
+  T maxDiff = 0.0;
+  T maxVal = 0.0;
+  for (std::size_t i=0;i < x.size();++i)
+  {
+    if (std::fabs(diff[i]) >std::fabs(maxDiff))
+    {
+      maxDiff = std::fabs(diff[i]);
+    }
+    if (std::fabs(x[i]) > std::fabs(maxVal))
+    {
+      maxVal = std::fabs(x[i]);
+    }
+  }
+  if (maxVal == 0.0)
+  {
+    return maxDiff;
+  }
+  return maxDiff/maxVal;
+}
+
+template<typename T>
+struct ImpreciseComp{
+  bool operator()(const std::vector<T>& x, const std::vector<T>& y) const
+  {
+    if (relativeError(x,y) < epsilon)
+    {
+      return false;
+    }
+    return x < y;
+  }
+};
+
+// typedef int NumberType;  // this probably isn't going to work
+// typedef std::vector<NumberType> VectorType;
+// typedef std::vector<VectorType> GeneratorList;
+// typedef std::set<VectorType> Orbit;
+
+typedef double NumberType;  // this probably isn't going to work
 typedef std::vector<NumberType> VectorType;
 typedef std::vector<VectorType> GeneratorList;
-typedef std::set<VectorType> Orbit;
+typedef std::set<VectorType, ImpreciseComp<NumberType>> Orbit;
 
 void out(std::ostream& file,const Orbit& solution, bool outputOrbit);
 
@@ -278,6 +370,7 @@ VectorType mirror(const VectorType& v, const VectorType& plane)
 
 void recorbit(const GeneratorList& generators, const VectorType& v, Orbit& history)
 {
+  //out(std::cout, history, true);
   if(history.find(v) != history.end())
   {
     return;
