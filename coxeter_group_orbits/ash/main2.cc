e0017266b44e1dbe7c47d804d22436fcb5f5832a
anne-sophie <anne-sophie@mailbox.tu-berlin.de>
2015-06-21 12:25:11 +0200

    testcases to check orbit-size if point lies on hyperplanes

--- coxeter_group_orbits/ash/main2.cc
+++ coxeter_group_orbits/ash/main2.cc
@@ -5,5 +5,14 @@
 #include "test_orbitSize.h"
 
 int main(int arg, const char* argv[]){
+	checkOrbitSize('B', 2, 2);
+	checkOrbitSize('B', 3, 3);
 	checkOrbitSize('B', 4, 2);
+	checkOrbitSize('B', 4, 3);
+	checkOrbitSize('B', 5, 2);
+	checkOrbitSize('B', 5, 3);
+	checkOrbitSize('B', 5, 4);
+	checkOrbitSize('B', 6, 3);
+	checkOrbitSize('B', 7, 2);
+	checkOrbitSize('B', 7, 3);
 }
