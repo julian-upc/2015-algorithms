96f252d8c846319b2ac2e7e57b519eff8c74a699
Anne-Sophie Himmel <anne-sophie.himmel@campus.tu-berlin.de>
2015-07-01 16:18:08 +0200

    fixed <-/==-method for ImpreciseVector

--- coxeter_group_orbits/ash/types.h
+++ coxeter_group_orbits/ash/types.h
@@ -21,6 +21,7 @@
 #include <numeric>
 #include <vector>
 #include <functional>
+#include <iostream>
 #include <initializer_list>
 #include <math.h>
 
@@ -28,6 +29,7 @@ class NotImplementedException : public std::exception {};
 class InvalidGroupException : public std::exception {};
 
 typedef long double NumberType;
+const static NumberType epsilon = 1.0E-14;
 
 class ImpreciseVector : public std::vector<NumberType>
 {
@@ -37,23 +39,31 @@ public:
    ImpreciseVector(const std::vector<NumberType>& v) : std::vector<NumberType>(v) {};
    ImpreciseVector(std::initializer_list<NumberType> v) : std::vector<NumberType>(v) {};
 
-   friend bool operator <(const ImpreciseVector& v1, const ImpreciseVector& v2 )
+   bool operator <(const ImpreciseVector& v1) const
    {
-      NumberType  epsilon =1.0E-3;
-
       for (std::vector<NumberType>::size_type i = 0; i != v1.size(); i++){
-        if(  abs(v1[i] - v2[i]) >  epsilon){
-           return static_cast<std::vector<NumberType>>(v1) < static_cast<std::vector<NumberType>>(v2);
-	}
+           if( (*this)[i] <  (v1[i] - epsilon)){
+              return true;
+	   }else{
+              if( (*this)[i] >  v1[i] + epsilon){
+                 return false;
+           }
+         }
       }
       return false;
-/*      if(sqrt(std::inner_product(tmp.begin(), tmp.end(), tmp.begin(), 0.0) ) < epsilon) 
-      //if(sqrt(std::inner_product(tmp.begin(), tmp.end(), tmp.begin(), 0.0) / (std::inner_product(v1.begin(), v1.end(), v1.begin(), 0.0) * std::inner_product(v2.begin(), v2.end(), v2.begin(), 0.0))) < epsilon) */
+   }
+
+   bool operator ==(const ImpreciseVector& v1 ) const
+   {
+      for (std::vector<NumberType>::size_type i = 0; i != v1.size(); i++){
+        if(  fabs(v1[i] - (*this)[i]) >  epsilon){
+           return false; 
+	}
+      }
+      return true;
    }
 };
 
-
-typedef std::vector<NumberType> CoxeterVectorType;
 typedef ImpreciseVector VectorType; //std::vector<NumberType, ImpreciseComp> VectorType;//
 typedef std::set<VectorType> Orbit;
 
